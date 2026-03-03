ea32f0fa349a2c0f3f1064399eb54107cf2e6391
M-SE0K <seg082911@gmail.com>
2026-03-03 13:32:26 +0900

    test: Added Refactor pass "Apply spherical template"

--- src/Escargot.h
+++ src/Escargot.h
@@ -599,7 +599,7 @@ using HashSet = tsl::robin_set<Key, Hash, KeyEqual, Allocator, StoreHash, Growth
 
 template <class Key, class T, class Hash = std::hash<Key>,
           class KeyEqual = std::equal_to<Key>,
-          class Allocator = std::allocator<std::pair<Key, T>>,
+          class Allocator = std::allocator<std::pair<Key, T> >,
           bool StoreHash = false,
           class GrowthPolicy = tsl::rh::mod_growth_policy<std::ratio<5, 4>>>
 using HashMap = tsl::robin_map<Key, T, Hash, KeyEqual, Allocator, StoreHash, GrowthPolicy>;
@@ -608,14 +608,14 @@ template <class Key, class Hash = std::hash<Key>,
           class KeyEqual = std::equal_to<Key>,
           class Allocator = std::allocator<Key>,
           bool StoreHash = false,
-          class GrowthPolicy = tsl::rh::power_of_two_growth_policy<2>>
+          class GrowthPolicy = tsl::rh::power_of_two_growth_policy<2> >
 using HashSet = tsl::robin_set<Key, Hash, KeyEqual, Allocator, StoreHash, GrowthPolicy>;
 
 template <class Key, class T, class Hash = std::hash<Key>,
           class KeyEqual = std::equal_to<Key>,
-          class Allocator = std::allocator<std::pair<Key, T>>,
+          class Allocator = std::allocator<std::pair<Key, T> >,
           bool StoreHash = false,
-          class GrowthPolicy = tsl::rh::power_of_two_growth_policy<2>>
+          class GrowthPolicy = tsl::rh::power_of_two_growth_policy<2> >
 using HashMap = tsl::robin_map<Key, T, Hash, KeyEqual, Allocator, StoreHash, GrowthPolicy>;
 #endif
 } // namespace Escargot
